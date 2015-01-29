a60ee953a7266285a014f2d44a84bcdc270ef8d2
cguzman <cguzman@localhost.localdomain>
2015-01-28 22:05:18 -0500

    Test class

---
+++ src/main/java/co/cg/apps/test/testgithub/TestClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.cg.apps.test.testgithub;
+
+/**
+ *
+ * @author cguzman
+ */
+public class TestClass {
+ 
+    public static void main (String[] arg) {
+       
+        System.out.println("Hola mundo");
+    }
+}
